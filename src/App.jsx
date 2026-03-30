d98788900991815818365a7b82a4ea1db17ecc52
zainsajjad903 <zainsajjad.903@gmail.com>
2026-03-30 09:59:55 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return <h1 className="display-4">Welcome to Our Store!</h1>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/products" element={<Product />} />
+      <Route path="/products/:id" element={<Singleproduct />} />
+    </div>
+  );
+};
 
 export default App;
