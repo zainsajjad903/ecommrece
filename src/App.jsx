dc484945035fa69e181c4abe77952fbb089fba56
zainsajjad903 <zainsajjad.903@gmail.com>
2026-03-30 10:04:44 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Singleproduct from "./pages/Singleproduct";
 const App = () => {
   return (
     <div className="App">
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/products" element={<Product />} />
-      <Route path="/products/:id" element={<Singleproduct />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/singleproduct" element={<Singleproduct />} />
+      </Routes>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Welcome to Our Store!</h1>
+    </div>
+  );
 }
