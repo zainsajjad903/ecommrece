73317be94102ec26296567790d9f2a4516734c6f
zainsajjad903 <zainsajjad.903@gmail.com>
2026-03-31 12:16:05 +0500

    Updated design and fixed bugs

--- src/Components/addproduct.jsx
+++ src/Components/addproduct.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import ProductForm from "../Productform";
 
-const Addproduct = (props) => {
-  function fetchProduct(Formproduct) {
-    props.getproduct(Formproduct);
-  }
+const Addproduct = ({ products }) => {
   return (
-    <div>
-      <ProductForm fetchProduct={fetchProduct} />
+    <div className="container">
+      <div className="row">
+        {products.map((item) => {
+          return (
+            <div className="col-lg-4" key={item.productId}>
+              <div className="item">
+                <div className="down-content">
+                  <h4>{item.productName}</h4>
+                  <span>{item.productPrice}</span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
