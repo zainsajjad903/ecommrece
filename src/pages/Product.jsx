fd772e95f5f0d1a0f6678a6eb7ef89b1bbb3a68f
zainsajjad903 <zainsajjad.903@gmail.com>
2026-03-31 10:28:31 +0500

    Updated design and fixed bugs

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,10 +4,13 @@ import Navbar from "../Components/Navbar";
 import ProductForm from "../Components/productform";
 import Addproduct from "../Components/productsview";
 const Product = () => {
+  function getproduct(get_product) {
+    console.log("this is product page", get_product);
+  }
   return (
     <>
       <Navbar />
-      <Addproduct />
+      <Addproduct getproduct={getproduct} />
       <ProductForm />
       <Footer />
     </>
