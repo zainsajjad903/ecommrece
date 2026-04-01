d92d81f078fc2b8d145be06f7c4dde243f27eb47
zainsajjad903 <zainsajjad.903@gmail.com>
2026-04-01 11:39:08 +0500

    Updated design and fixed bugs

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,63 +1,63 @@
 import { useState } from "react";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
-import ProductForm from "../Components/productform";
+
 import ProductArea from "../Components/ProductArea";
 import AddProduct from "../Components/AddProduct";
 let Item = [
   {
-    productId: 1,
+    productId: 2,
     productName: "Classic Spring",
     productPrice: "$120.00",
     productImage: "assets/images/men-01.jpg",
   },
 
   {
-    productId: 2,
+    productId: 3,
     productName: "Air Force 1 X",
     productPrice: "$90.00",
     productImage: "assets/images/men-02.jpg",
   },
   {
-    productId: 3,
+    productId: 4,
     productName: "Love Nana ‘20",
     productPrice: "$150.00",
     productImage: "assets/images/men-03.jpg",
   },
   {
-    productId: 4,
+    productId: 5,
     productName: "New Green Jacket",
     productPrice: "$75.00",
     productImage: "assets/images/women-01.jpg",
   },
 
   {
-    productId: 5,
+    productId: 6,
     productName: "Classic Dress",
     productPrice: "$45.00",
     productImage: "assets/images/women-02.jpg",
   },
   {
-    productId: 6,
+    productId: 7,
     productName: "Spring Collection",
     productPrice: "$130.00",
     productImage: "assets/images/women-03.jpg",
   },
   {
-    productId: 7,
+    productId: 8,
     productName: "School Collection",
     productPrice: "$80.00",
     productImage: "assets/images/kid-01.jpg",
   },
 
   {
-    productId: 8,
+    productId: 9,
     productName: "Summer Cap",
     productPrice: "$12.00",
     productImage: "assets/images/kid-02.jpg",
   },
   {
-    productId: 9,
+    productId: 10,
     productName: "Classic Kid",
     productPrice: "$30.00",
     productImage: "assets/images/kid-03.jpg",
